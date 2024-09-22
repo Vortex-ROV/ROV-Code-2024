ce5922b2f544a1ce8f25ba88324a0d466885b448
AlyEzzEldin <aliezz915@gmail.com>
2024-09-22 20:01:16 +0300

    Removed unnecessary comments

--- src/oakD/Server.py
+++ src/oakD/Server.py
@@ -66,24 +66,6 @@ class oakServer():
             print("Exiting...")
             netgear_stream.stop()
 
-    
-    # def Main(self):
-    #     # Create the Netgear server
-    #     server =  NetgearServer()
-    #         # Create the Oak pipeline
-    #     pipeline = OakPipeline(FPS=60).get_pipeline()
-    #     # Connect to device and start pipeline
-    #     with dai.Device(pipeline) as device:
-    #         video = device.getOutputQueue(name="video", maxSize=1, blocking=False)
-    #         try:
-    #             while True:
-    #                 videoIn = video.get()
-    #                 server.send(videoIn.getCvFrame())
-    #         except RuntimeError:
-    #             server.close()
-
-
-
 if __name__ == "__main__":
     camera = oakServer()
     oakServer.main()
