d676a0ab193cfd0d18db380a5cc57f6a3745a56e
rov <rov@orcas.vortex>
2024-11-26 19:25:05 +0200

    autorun changes

--- autorun.sh
+++ autorun.sh
@@ -3,4 +3,4 @@ export OPENBLAS_CORETYPE=ARMV8
 export PYTHONPATH=/home/rov/.local/lib/python3.6/site-packages
 export PATH="${PATH}:/home/rov/.local/bin"
 /usr/bin/python3.6 /home/rov/ROV-Code/main.py &
-/usr/bin/python3.6 /home/rov/.local/bin/mavproxy.py --master /dev/ttyACM0 --out 192.168.33.100:14550
+/usr/bin/python3.6 /home/rov/.local/bin/mavproxy.py --master /dev/ttyACM0 --out 192.168.33.100:14550 --daemon
