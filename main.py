aac6e688b23c49daa07603998a3b15627fee3381
rov <rov@orcas.vortex>
2025-02-05 19:32:00 +0200

    Removed Pixhawk modules

--- main.py
+++ main.py
@@ -6,33 +6,9 @@ from src.pixhawk import Pixhawk
 from src.oakD import oakServer
 
 oak = oakServer()
-Sobek = ServerSocket(4096)
-Sobek.accept()
 
-while True:
-    try:
-        pix = Pixhawk()
-        print("done")
-        break
-    except Exception as e:
-        print(e)
-        print("No pixhawk connected")
-
-Heeartbeat_thread = threading.Thread(target=pix.heartbeat)
 oak_thread = threading.Thread(target=oak.main)
-Heeartbeat_thread.start()
 oak_thread.start()
 
-msg_len = len(Message().bytes())
-
 while True:
-    
-    msg = Sobek.receive(msg_len)
-
-    # Sobek.send(pix.get_sensor().encode())
-
-    if msg is not None:
-        pix.control_pixhawk(msg)
-        print(msg)
-
     time.sleep(0.01)
